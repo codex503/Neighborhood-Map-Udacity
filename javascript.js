b5626e21ae1b17919d2b47549bcb44831544f822
Derek Lee <dleedev7@gmail.com>
2017-01-04 14:44:52 -0500

    fixed issue with map display

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
-var map;
+ var map;
       function initMap() {
-        // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-          center: {lat: 40.7413549, lng: -73.9980244}
-          
+          center: {lat: 40.700936, lng: -73.712909},
+          zoom: 14
         });
       }
